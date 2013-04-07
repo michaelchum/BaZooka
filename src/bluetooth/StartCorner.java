bfa736ac6fc2b08cd8b6e601b009207ab62ec18c
Clark Gredona <cgredona@gmail.com>
2013-04-07 16:18:40 -0400

    Modified StartCorner as per the most recent fix on myCourses (now reflects the specs

--- src/bluetooth/StartCorner.java
+++ src/bluetooth/StartCorner.java
@@ -7,15 +7,15 @@ package bluetooth;
 
 /**
  * BOTTOM LEFT	X1 (0,0) "BL"
- * TOP LEFT		X2 (0,10) "TL"
+ * BOTTOM RIGHT	X2 (10,0) "BR"
  * TOP RIGHT	X3 (10,10) "TR"
- * BOTTOM RIGHT	X4 (10,0) "BR"
+ * TOP LEFT		X4 (0,10) "TL"
  */
 public enum StartCorner {
 	BOTTOM_LEFT(1,0,0, "BL"),
-	TOP_LEFT(2,0,10, "TL"),
+	BOTTOM_RIGHT(2,0,10, "BR"),
 	TOP_RIGHT(3,10,10, "TR"),
-	BOTTOM_RIGHT(4,10,0, "BR"),
+	TOP_LEFT(4,10,0, "TL"),
 	NULL(0,0,0, "NULL");
 	
 	private int id, x, y;
